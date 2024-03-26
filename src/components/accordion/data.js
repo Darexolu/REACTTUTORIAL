0e7faa969d42fc13ce226bc659cc4b88e63ab722
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2024-03-26 10:06:31 +0100

    first commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordion component?",
+    answer:
+      "they are user interface",
+  },
+  {
+    id: "2",
+    question: "Can i create an accordion component with another framework?",
+    answer:
+      "Yes of course, it is very possible to create an accordion component with another framework",
+  },
+  {
+    id: "3",
+    question: "With what framework?",
+    answer:
+      "Any framework of your choice",
+  },
+  {
+    id: "4",
+    question: "how is it going to look like?",
+    answer:
+      "The same way",
+  },
+];
+
+export default data;
